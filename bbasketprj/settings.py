fdc3113e5ca43e113138a987d28e6f8e0df12eba
sejin <sejinjiji@gmail.com>
2023-02-18 17:53:42 +0900

    plz god

--- bbasketprj/settings.py
+++ bbasketprj/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
